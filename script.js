3ce59da3c6ca0b990722284482a34f74f92a5d51
Ansh Patel <anshdpatel10@gmail.com>
2021-06-27 15:17:25 +0530

    feat: Add etch-a-sketch body background and add reset button, with minor design changes for all buttons

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const white = document.querySelector("#white");
 black.addEventListener('click', blackFill);
 white.addEventListener('click', whiteFill);
 random.addEventListener('click', randomFill);
- 
+reset = document.querySelector("#reset");
+reset.addEventListener('click', resetGrid);
  
  
  
@@ -21,11 +22,14 @@ function getNewDimensions()
  
 function generateGrid(dimension)
 {
+    normalButtons();
    for(let i = 0; i < dimension ** 2; i++)
    {
        const div = document.createElement('div');
        div.classList = "squares";
-       div.style.border = '1px solid black';
+       div.style.border = '1px solid #D3D3D3';
+       div.style.borderRadius = "10%";
+       div.style.background = "white";
        container.appendChild(div);
    }
    container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
@@ -41,6 +45,11 @@ function removeGrid()
  
 function blackFill(e)
 {
+    const whiteButton = document.querySelector("#white");
+    const randomButton = document.querySelector("#random");
+    whiteButton.style.background = "white";
+    randomButton.style.background = "white";
+    e.target.style.background = "silver";
    const squares = container.querySelectorAll('.squares');
    squares.forEach(square => square.addEventListener('mouseover', function(e){
        square.style.background = "black";
@@ -49,6 +58,11 @@ function blackFill(e)
  
 function whiteFill(e)
 {
+    const blackButton = document.querySelector("#black");
+    const randomButton = document.querySelector("#random");
+    blackButton.style.background = "white";
+    randomButton.style.background = "white";
+    e.target.style.background = "silver";
    const squares = container.querySelectorAll('.squares');
    squares.forEach(square => square.addEventListener('mouseover', function(e){
        square.style.background = "white";
@@ -57,13 +71,36 @@ function whiteFill(e)
  
 function randomFill(e)
 {
+    const blackButton = document.querySelector("#black");
+    const whiteButton = document.querySelector("#white");
+    blackButton.style.background = "white";
+    whiteButton.style.background = "white";
+    e.target.style.background = "silver";
    const squares = container.querySelectorAll('.squares');
+   console.log(squares);
    squares.forEach(square => square.addEventListener('mouseover', () => {
        let redColor = Math.floor(255* Math.random());
        let greenColor = Math.floor(255* Math.random());
        let blueColor = Math.floor(255* Math.random());
        square.style.background = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
    }));
+}
+
+function resetGrid()
+{
+    const squares = container.querySelectorAll('.squares');
+    console.log(squares);
+    squares.forEach(square => square.style.background = "white");
+}
+
+function normalButtons() //To remove the background color from the buttons when a new grid is created so that the buttons dont look selected
+{
+    const blackButton = document.querySelector("#black");
+    const randomButton = document.querySelector("#random");
+    const whiteButton = document.querySelector("#white");
+    blackButton.style.background = "white";
+    randomButton.style.background = "white";
+    whiteButton.style.background = "white";
 }
 
 
